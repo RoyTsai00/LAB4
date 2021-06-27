ca9da101d126c6c7bb3360a75b6521d3c308c9a5
RoyTsai00 <s2250089056@gmail.com>
2021-06-27 16:56:54 +0800

    add file

---
+++ HelloEclipse.java
@@ -0,0 +1,27 @@
+package aaa;
+
+public class HelloEclipse {
+	
+	public static void main(String[] args){
+		
+//    	for (int i =1; i <=3; i++) {
+//        print99();
+//    	}
+    }
+//    public static void print99() {
+//    	int i, j;
+//		for (i = 1; i <= 9; i++) {
+//			for (j = 1; j <= 9; j++)
+//				System.out.print(i + "*" + j + "=" + i * j + "\t");
+//			System.out.println();
+//			
+//		}
+//		System.out.println("=================");
+//		return; //可加上,因為沒有回傳任何東西 
+//    }
+
+    	
+    	
+    }
+    
+
